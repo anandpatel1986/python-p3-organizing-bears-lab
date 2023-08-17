81ffcb245e57fa0c93923def9eaed50ca7411799
Anandkumar Patel <avpatel2005@gmail.com>
2023-08-16 20:17:26 -0400

    completes lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ("Mr. Chocolate",2,"M", NULL, NULL,NULL);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy",5,"M", NULL, NULL,NULL);
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ("Tabitha",4,"F", NULL, NULL,NULL);
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ("Sergeant Brown",2,"M", NULL, NULL,NULL);
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ("Melissa",6,"F", NULL, NULL,NULL);
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ("Grinch",2,"M", NULL, NULL,NULL);
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES ("Wendy",3,"F", NULL, NULL,NULL);
+INSERT INTO bears (name, age, sex, color, temperament, alive ) VALUES (NULL,4,"M", NULL, NULL,NULL);
